78d7c8be3dbf6e763cefca49ae0064f99ff3086e
AjayBarot7035 <ajubarot@gmail.com>
2024-11-24 09:11:46 +0530

    Fix the indentation for the rdoc documentation

--- lib/calcio.rb
+++ lib/calcio.rb
@@ -1,7 +1,7 @@
 class Calcio
-	#
-	# @param input [String] [input data]
-	#
+  #
+  #@param input [String] [input data]
+  #
   def self.add(input)
     return 0 if input.strip.empty?
     input.split(',').map(&:to_i).reduce(:+)
